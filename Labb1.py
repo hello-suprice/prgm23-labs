0930457ba3844837b9f08f120e44b63444fecf6d
Teeshk Nader <90448303+hello-suprice@users.noreply.github.com>
2023-09-05 16:16:59 +0200

    Create Labb1.py
    labb 1 klar

---
+++ Labb1.py
@@ -0,0 +1,38 @@
+# Funktion för att beräkna summan av en aritmetisk talföljd
+def aritmetisk_summa(a1, d, n):
+    '''
+    Beräknar summan av en aritmetisk talföljd med det första termvärdet a_1,
+    differensen d mellan termerna och antalet termer n, enligt formeln för aritmetisk summa.
+    '''
+    summa = n * (2 * a1 + (n - 1) * d) / 2 #Formeln för arimetisk summa med aritmetisk följd för sista talet i följden
+    return summa
+
+# Funktion för att beräkna summan av en geometrisk talföljd
+def geometrisk_summa(g1, q, n):
+    '''
+    Beräknar summan av en geometrisk talföljd med det första termvärdet g_1,
+    kvoten q mellan termerna och antalet termer n, enligt formeln för geometrisk summa.
+    '''
+    summa = g1 * (((g1 * q**(n-1)) - 1) / (q - 1))
+    return summa
+
+# Användaren matar in värden:
+a1 = float(input("Skriv in startvärdet (a1): \n"))
+d = float(input("Ange differensen (d) för den aritmetiska talföljden: \n"))
+n1 = int(input("Ange antalet termer (n) för den aritmetiska talföljden: \n"))
+
+# Beräkna och skriv ut resultaten
+aritmetisk_resultat = aritmetisk_summa(a1, d, n1)
+print("Den aritmetiska summan är: ", aritmetisk_resultat)
+
+g1 = float(input("Ange det första värdet (g_1) för den geometriska talföljden: \n"))
+q = float(input("Ange kvoten (q) för den geometriska talföljden: \n"))
+n2 = int(input("Ange antalet termer (n) för den geometriska talföljden: \n"))
+
+
+# Beräkna och skriv ut resultaten
+geometrisk_resultat = geometrisk_summa(g1, q, n2)
+print("Den geometriska summan är: ", geometrisk_resultat)
+
+    
+  
