fb691396ffa1ed7355cebec01c1e7b49cf5d1e1f
Teeshk Nader <90448303+hello-suprice@users.noreply.github.com>
2023-09-07 10:43:35 +0200

    Update Labb1.py
    fixade formeln för geometriska, och byte om variabelnamnen för input och tog bort onödiga kommentarer.

--- Labb1.py
+++ Labb1.py
@@ -1,4 +1,3 @@
-# Funktion för att beräkna summan av en aritmetisk talföljd
 def aritmetisk_summa(a1, d, n):
     '''
     Beräknar summan av en aritmetisk talföljd med det första elementvärdet a_1,
@@ -7,32 +6,31 @@ def aritmetisk_summa(a1, d, n):
     summa = n * (2 * a1 + (n - 1) * d) / 2 #Formeln för arimetisk summa med aritmetisk följd för sista elementet i följden
     return summa
 
-# Funktion för att beräkna summan av en geometrisk talföljd
 def geometrisk_summa(g1, q, n):
     '''
     Beräknar summan av en geometrisk talföljd med det första elementvärdet g_1,
     kvoten q mellan elementerna och antalet element n, enligt formeln för geometrisk summa.
     '''
-    summa = g1 * (((g1 * q**(n-1)) - 1) / (q - 1)) #Formeln för geometrisk summa med geometrisk följd för sista elementet i följden
+    summa = ( g1 * ((q**(n)) - 1) / (q - 1)) #Formeln för geometrisk summa med geometrisk följd för sista elementet i följden
     return summa
 
 # Användaren matar in värden:
-a1 = float(input("Skriv in startvärdet (a1): \n"))
-d = float(input("Ange differensen (d) för den aritmetiska talföljden: \n"))
-n1 = int(input("Ange antalet elementer (n) för den aritmetiska talföljden: \n"))
+start_värde_aritmetisk = float(input("Ange det första värdet för den geometriska talföljden: \n"))
+diffrens = float(input("Ange differensen för den aritmetiska talföljden: \n"))
+antalet_element_aritmetisk = int(input("Ange antalet elementer för den aritmetiska talföljden: \n"))
 
 # Beräkna och skriv ut resultaten
-aritmetisk_resultat = aritmetisk_summa(a1, d, n1)
+aritmetisk_resultat = aritmetisk_summa(start_värde_aritmetisk, diffrens, antalet_element_aritmetisk)
 print("Den aritmetiska summan är: ", aritmetisk_resultat)
 
 # Användaren matar in värden:
-g1 = float(input("Ange det första värdet (g_1) för den geometriska talföljden: \n"))
-q = float(input("Ange kvoten (q) för den geometriska talföljden: \n"))
-n2 = int(input("Ange antalet elementer (n) för den geometriska talföljden: \n"))
+start_värde_geometrisk = float(input("Ange det första värdet för den geometriska talföljden: \n"))
+kvot = float(input("Ange kvoten för den geometriska talföljden: \n"))
+antal_element_geometrisk = int(input("Ange antalet elementer för den geometriska talföljden: \n"))
 
 
 # Beräkna och skriv ut resultaten
-geometrisk_resultat = geometrisk_summa(g1, q, n2)
+geometrisk_resultat = geometrisk_summa(start_värde_geometrisk, kvot, antal_element_geometrisk)
 print("Den geometriska summan är: ", geometrisk_resultat)
 
     
