0b52480518daf488440d17c12754c77aa8434e38
Teeshk Nader <90448303+hello-suprice@users.noreply.github.com>
2023-09-07 10:52:21 +0200

    2 Labb1.py

--- Labb1.py
+++ Labb1.py
@@ -3,7 +3,7 @@ def aritmetisk_summa(a1, d, n):
     Beräknar summan av en aritmetisk talföljd med det första elementvärdet a_1,
     differensen d mellan elementerna och antalet elementer n, enligt formeln för aritmetisk summa.
     '''
-    summa = n * (2 * a1 + (n - 1) * d) / 2 #Formeln för arimetisk summa med aritmetisk följd för sista elementet i följden
+    summa = n * (2 * a1 + (n - 1) * d) / 2
     return summa
 
 def geometrisk_summa(g1, q, n):
@@ -11,7 +11,7 @@ def geometrisk_summa(g1, q, n):
     Beräknar summan av en geometrisk talföljd med det första elementvärdet g_1,
     kvoten q mellan elementerna och antalet element n, enligt formeln för geometrisk summa.
     '''
-    summa = ( g1 * ((q**(n)) - 1) / (q - 1)) #Formeln för geometrisk summa med geometrisk följd för sista elementet i följden
+    summa = ( g1 * ((q**(n)) - 1) / (q - 1))
     return summa
 
 # Användaren matar in värden för aritmetiska summan:
