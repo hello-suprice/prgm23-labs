784dc66ef7d9722ff6b542192a624da7cacc7e83
Teeshk Nader <90448303+hello-suprice@users.noreply.github.com>
2023-09-30 04:51:05 +0200

    klar

--- Labb 4.py
+++ Labb 4.py
@@ -1,13 +1,25 @@
 class Student:
-    def __init__(self, first_name, last_name, personnummer):
-        self.first_name = first_name
-        self.last_name = last_name
+    """
+    En klass för att representera en student.
+    """
+    def __init__(self, fornamn, efternamn, personnummer):
+        """
+        Konstruktorn för klassen Student.
+        """
+        self.fornamn = fornamn
+        self.efternamn = efternamn
         self.personnummer = personnummer
 
     def __str__(self):
-        return f"Namn: {self.first_name} {self.last_name} Personnr: {self.personnummer}"
+        """
+        Returnerar en strängrepresentation av studentobjektet.
+        """
+        return f"Namn: {self.fornamn} {self.efternamn} Personnr: {self.personnummer}"
 
-def get_valid_personnummer():
+def hämta_giltigt_personnummer():
+    """
+    Funktion för att få ett giltigt personnummer från användaren.
+    """
     while True:
         personnummer = input("Vad är studentens personnummer? ")
         if personnummer.isdigit() and len(personnummer) == 10:
@@ -15,20 +27,44 @@ def get_valid_personnummer():
         else:
             print("Personnumret får bara innehålla 10 siffror, försök igen!")
 
-def add_student():
+def lägg_till_student():
+    """
+    Funktion för att lägga till en ny student.
+    """
     print("Lägg till en ny student:")
-    first_name = input("Förnamn: ")
-    last_name = input("Efternamn: ")
-    personnummer = get_valid_personnummer()
-    student = Student(first_name, last_name, personnummer)
-    students.append(student)
+    fornamn = input("Förnamn: ")
+    efternamn = input("Efternamn: ")
+    personnummer = hämta_giltigt_personnummer()
+    student = Student(fornamn, efternamn, personnummer)
+    studenter.append(student)
     print("Objektet skapat!\n")
 
-def update_student():
+def lägg_till_studenter():
+    """
+    Funktion för att lägga till flera studenter.
+    """
+    antal_studenter = int(input("Hur många studenter vill du lägga till? "))
+    for _ in range(antal_studenter):
+        lägg_till_student()
+
+def ta_bort_student():
+    """
+    Funktion för att ta bort en befintlig student.
+    """
+    personnummer_att_ta_bort = input("Skriv in personnumret på objektet du vill ta bort: ")
+    for student in studenter:
+        if student.personnummer == personnummer_att_ta_bort:
+            studenter.remove(student)
+            print(f"Objektet med personnummer {personnummer_att_ta_bort} har tagits bort!")
+            break
+    else:
+        print("Inget objekt med det personnumret hittades.")
+
+def uppdatera_student():
     personnummer_to_update = input("Skriv in personnumret på objektet du vill ändra: ")
-    for student in students:
+    for student in studenter:
         if student.personnummer == personnummer_to_update:
-            choice = input(print(f"Vill du ändra namn på {student.first_name} {student.last_name} (Ja eller nej)?"))
+            choice = input(print(f"Vill du ändra namn på {student.first_name} {student.last_name} (Ja eller nej)? "))
             if choice.lower() == 'ja':
                 new_first_name = input("Skriv in det nya förnamnet: ")
                 new_last_name = input("Skriv in det nya efternamnet: ")
@@ -42,32 +78,32 @@ def update_student():
     else:
         print("Inget objekt med det personnumret hittades.")
 
-def remove_student():
-    personnummer_to_remove = input("Skriv in personnumret på objektet du vill ta bort: ")
-    for student in students:
-        if student.personnummer == personnummer_to_remove:
-            students.remove(student)
-            print(f"Objektet med personnummer {personnummer_to_remove} har tagits bort!")
-            break
-    else:
-        print("Inget objekt med det personnumret hittades.")
-
-students = []
+studenter = []
 
 while True:
-    print("Vill du lägga till (l), ändra (a), ta bort (t) ett objekt eller avsluta (q)?")
-    choice = input()
-    if choice.lower() == 'l':
-        add_student()
-    elif choice.lower() == 'a':
-        update_student()
-    elif choice.lower() == 't':
-        remove_student()
-    elif choice.lower() == 'q':
+    print("""
+          Välj ett alternativ från menyn nedan:
+          (l) Lägg till en ny student
+          (m) Lägg till flera nya studenter
+          (t) Ta bort en befintlig student
+          (a) Ändra en befintligt student
+          (q) Avsluta programmet
+          """)
+
+    val = input()
+    if val.lower() == 'l':
+        lägg_till_student()
+    elif val.lower() == 'm':
+        lägg_till_studenter()
+    elif val.lower() == 't':
+        ta_bort_student()
+    elif val.lower() == 'a':
+        uppdatera_student()
+    elif val.lower() == 'q':
         break
     else:
         print("Ogiltigt val. Försök igen.")
 
 print("Här är alla sparade objekt:")
-for student in students:
+for student in studenter:
     print(student)
--- test.py
+++ test.py
@@ -1,128 +1,60 @@
-import typed_input
+class Student:
+    def __init__(self, first_name, last_name, personnummer):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.personnummer = personnummer
 
-def aritmetisk_summa(a1, d, n):
-    return n * (2 * a1 + (n - 1) * d) / 2
+    def __str__(self):
+        return f"Namn: {self.first_name} {self.last_name} Personnr: {self.personnummer}"
 
-def geometrisk_summa(g1, q, n):
-    if q == 1:
-        raise ValueError("Kvoten kan inte vara 1 för en geometrisk summa.")
-    return g1 * ((q**n) - 1) / (q - 1)
-
-def läs_in_talföljd():
-   
+def get_valid_personnummer():
     while True:
-        talföljd_typ = input("Är talföljden [a]ritmetisk eller [g]eometrisk? ").lower()
-        if talföljd_typ not in ('a', 'g'):
-            print("Ogiltigt val. Välj 'a' för aritmetisk eller 'g' för geometrisk.")
+        personnummer = input("Vad är studentens personnummer? ")
+        if personnummer.isdigit() and len(personnummer) == 10:
+            return personnummer
         else:
+            print("Personnumret får bara innehålla 10 siffror, försök igen!")
+
+def add_student():
+    print("Lägg till en ny student:")
+    first_name = input("Förnamn: ")
+    last_name = input("Efternamn: ")
+    personnummer = get_valid_personnummer()
+    student = Student(first_name, last_name, personnummer)
+    students.append(student)
+    print("Objektet skapat!\n")
+
+def add_students():
+    antal_studenter = int(input("Hur många studenter vill du lägga till? "))
+    for _ in range(antal_studenter):
+        add_student()
+
+def remove_student():
+    personnummer_to_remove = input("Skriv in personnumret på objektet du vill ta bort: ")
+    for student in students:
+        if student.personnummer == personnummer_to_remove:
+            students.remove(student)
+            print(f"Objektet med personnummer {personnummer_to_remove} har tagits bort!")
             break
-    
-    if talföljd_typ == 'a':
-        följd = 'aritmetisk'
     else:
-        följd = 'geometrisk'
-    start_värde = typed_input.läs_in_flyttal(f"Skriv in startvärdet för den {följd} talföljden: ")
-    
-    if talföljd_typ == 'a':
-        diffrens = typed_input.läs_in_flyttal("Skriv in differensen: ")
-        
+        print("Inget objekt med det personnumret hittades.")
+
+students = []
+
+while True:
+    print("Vill du lägga till (l) en student, lägga till (m) flera studenter, ta bort (t) en student eller avsluta (q)?")
+    choice = input()
+    if choice.lower() == 'l':
+        add_student()
+    elif choice.lower() == 'm':
+        add_students()
+    elif choice.lower() == 't':
+        remove_student()
+    elif choice.lower() == 'q':
+        break
     else:
-        kvot = typed_input.läs_in_flyttal("Skriv in kvoten: ")
-        while kvot == 0 or kvot == 1:
-            print("Kvoten kan inte vara 0 eller 1 för en geometrisk talföljd.")
-            kvot = typed_input.läs_in_flyttal("Skriv in värdet på kvoten igen: ")
-    
-    
-    return talföljd_typ, start_värde, diffrens if talföljd_typ == 'a' else kvot
+        print("Ogiltigt val. Försök igen.")
 
-def jämför_summor():
-    print("Data för den första summan:")
-    talföljd_typ1, start_värde1, parameter1 = läs_in_talföljd()
-    
-
-    print("\nData för den andra summan:")
-    talföljd_typ2, start_värde2, parameter2 = läs_in_talföljd()
-    
-    
-    antal_element = typed_input.läs_in_heltal("\nSkriv in antalet element i följden: ")
-    
-    while antal_element <= 0:
-        print("Antalet element måste vara större än noll.")
-        antal_element = typed_input.läs_in_heltal("Skriv in värdet på antal element igen: ")
-
-    
-    if talföljd_typ1 == 'a':
-        resultat1 = aritmetisk_summa(start_värde1, parameter1, antal_element)
-        talföljd_typ1 = 'aritmetiska'
-    else:
-        resultat1 = geometrisk_summa(start_värde1, parameter1, antal_element)
-        talföljd_typ1 = 'geometriska'
-    if talföljd_typ2 == 'a':
-        resultat2 = aritmetisk_summa(start_värde2, parameter2, antal_element)
-        talföljd_typ2 = 'aritmetiska'
-    else:
-        resultat2 = geometrisk_summa(start_värde2, parameter2, antal_element)
-        talföljd_typ2 = 'geometriska'
-
-    if resultat1 > resultat2:
-        print(f"Den {talföljd_typ1} summan är störst.")
-    elif resultat1 < resultat2:
-        print(f"Den {talföljd_typ2} summan är störst.")
-    else:
-        print("Båda summorna är lika stora.")
-
-def meny():
-    print(
-        """
-        --- Meny ---
-        Välkommen till Summa Meny. Välj ett av alternativen nedan:
-        1. Räkna ut aritmetisk summa.
-        2. Räkna ut geometrisk summa.
-        3. Jämför två summor.
-        4. Avsluta.
-        """
-    )
-
-    val = typed_input.läs_in_heltal("Vad vill du göra? ")
-
-    while val != 4:
-        if val == 1:
-            start_värde_aritmetisk = typed_input.läs_in_flyttal("Ange det första värdet för den aritmetiska talföljden: ")
-            diffrens = typed_input.läs_in_flyttal("Ange differensen för den aritmetiska talföljden: ")
-            antalet_element_aritmetisk = typed_input.läs_in_heltal("Ange antalet elementer för den aritmetiska talföljden: ")
-            
-            while antalet_element_aritmetisk <= 0:
-                print("Antalet element måste vara större än noll.")
-                antalet_element_aritmetisk = typed_input.läs_in_heltal("Skriv in värdet på antalet element igen: ")
-
-            aritmetisk_resultat = aritmetisk_summa(start_värde_aritmetisk, diffrens, antalet_element_aritmetisk)
-            print("Den aritmetiska summan är:", aritmetisk_resultat)
-        
-        elif val == 2:
-            start_värde_geometrisk = typed_input.läs_in_flyttal("Ange det första värdet för den geometriska talföljden: ")
-            kvot = typed_input.läs_in_flyttal("Ange kvoten för den geometriska talföljden: ")
-            
-            while kvot == 0 or kvot == 1:
-                print("Kvoten kan inte vara 0 eller 1. Försök igen.")
-                kvot = typed_input.läs_in_flyttal("Skriv in värdet på kvoten igen: ")
-            
-            antal_element_geometrisk = typed_input.läs_in_heltal("Ange antalet elementer för den geometriska talföljden: ")
-            
-            while antal_element_geometrisk <= 0:
-                print("Antalet element måste vara större än noll.")
-                antal_element_geometrisk = typed_input.läs_in_heltal("Skriv in värdet på antalet element igen: ")
-
-            geometrisk_resultat = geometrisk_summa(start_värde_geometrisk, kvot, antal_element_geometrisk)
-            print("Den geometriska summan är:", geometrisk_resultat)
-        
-        elif val == 3:
-            jämför_summor()
-        
-        else:
-            print("Felaktigt val! Välj en siffra mellan 1 och 4.")
-
-        val = typed_input.läs_in_heltal("Vad vill du göra? ")
-
-    print("Avslutar")
-
-meny()
+print("Här är alla sparade objekt:")
+for student in students:
+    print(student)
