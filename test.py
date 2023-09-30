b889e750d187f133b3dcaf8b9258be887d67442d
Teeshk Nader <90448303+hello-suprice@users.noreply.github.com>
2023-09-30 23:50:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,60 +1,31 @@
+
+# Eugenia Pallares, Evangeline Hövenmark, Tua Jacobsson
+# 27.09.23
+# DD1310 HT23
+# Laboration 4: Klasser och objekt
+"""Välkommen! Detta program skapar objekt i klasser med tre olika attribut.
+Det finns även en modul som kör en felhantering för personnummer."""
+import typed_input
+lista_studenter = [] # tom lista skapas
 class Student:
-    def __init__(self, first_name, last_name, personnummer):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.personnummer = personnummer
-
+    def __init__(self, förnamn, efternamn, personnmr):
+        """Definerar attributen förnamn, efternamn och personnmr för objektet
+        Student."""
+        self.förnamn = förnamn
+        self.efternamn = efternamn
+        self.personnmr = personnmr
     def __str__(self):
-        return f"Namn: {self.first_name} {self.last_name} Personnr: {self.personnummer}"
-
-def get_valid_personnummer():
-    while True:
-        personnummer = input("Vad är studentens personnummer? ")
-        if personnummer.isdigit() and len(personnummer) == 10:
-            return personnummer
-        else:
-            print("Personnumret får bara innehålla 10 siffror, försök igen!")
-
-def add_student():
-    print("Lägg till en ny student:")
-    first_name = input("Förnamn: ")
-    last_name = input("Efternamn: ")
-    personnummer = get_valid_personnummer()
-    student = Student(first_name, last_name, personnummer)
-    students.append(student)
-    print("Objektet skapat!\n")
-
-def add_students():
-    antal_studenter = int(input("Hur många studenter vill du lägga till? "))
-    for _ in range(antal_studenter):
-        add_student()
-
-def remove_student():
-    personnummer_to_remove = input("Skriv in personnumret på objektet du vill ta bort: ")
-    for student in students:
-        if student.personnummer == personnummer_to_remove:
-            students.remove(student)
-            print(f"Objektet med personnummer {personnummer_to_remove} har tagits bort!")
-            break
-    else:
-        print("Inget objekt med det personnumret hittades.")
-
-students = []
-
-while True:
-    print("Vill du lägga till (l) en student, lägga till (m) flera studenter, ta bort (t) en student eller avsluta (q)?")
-    choice = input()
-    if choice.lower() == 'l':
-        add_student()
-    elif choice.lower() == 'm':
-        add_students()
-    elif choice.lower() == 't':
-        remove_student()
-    elif choice.lower() == 'q':
-        break
-    else:
-        print("Ogiltigt val. Försök igen.")
-
-print("Här är alla sparade objekt:")
-for student in students:
-    print(student)
+        """Returnerar en sträng som innehåller attributen"""
+        return ("Namn: " + self.förnamn + " " + self.efternamn + " Personnmr: " +(self.personnmr))
+i = 0
+while i < 3: # while-loop som frågar användaren om förnamn, efternamn och personnmr till objektet Student
+    p_förnamn = input("Vad är studentens förnamn? " )
+    p_efternamn = input("Vad är studentens efternamn? " )
+    p_personnmr = typed_input.felHeltal("Vad är studentens personnmr (skriv ÅÅMMDDXXXX)?" ) # kör modul felhantering
+    student = Student(p_förnamn, p_efternamn, p_personnmr)
+    lista_studenter.append(student) # lägger till strägen från klassen Student till listan
+    print("\nObjekt har skapats! \n")
+    i += 1
+    print("Här är alla sparade objekt:")
+for student in lista_studenter:
+    print(student) # skriver ut alla element i listan
