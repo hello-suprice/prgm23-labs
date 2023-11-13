c8c066876a2773b42e0c3a092498bbf1d51f0183
Teeshk Nader <90448303+hello-suprice@users.noreply.github.com>
2023-11-13 22:48:51 +0100

    ok

--- Salladbar.py
+++ Salladbar.py
@@ -25,25 +25,43 @@ class Salladbar():
 
     def __init__(self):
         self.sallader = []
-        self.ingreidenser = []
+        self.ingredienser = []
     
     def läs_in_sallader(self):
         """
         Läser in sallader från en JSON-fil och lagrar dem i en lista.
         """
-        pass
+        try:
+            with open('sallader.json', 'r', encoding='utf-8') as f:
+                sallader_data = json.load(f)
+                for sallad_data in sallader_data:
+                    namn_sallad = sallad_data["namn_sallad"]
+                    sallad_pris = sallad_data["sallad_pris"]
+                    ingredienser = sallad_data["ingredienser"]
+                    self.sallader.append(Sallad(namn_sallad, sallad_pris, ingredienser))
+        except FileNotFoundError:
+            print("Kunde inte hitta filen 'sallader.json'. Kontrollera att filen finns och försök igen.")
         
     def läs_in_ingredienser(self):
         """
         Läser in ingredienser från en JSON-fil och lagrar dem i en lista.
         """
-        pass
+        try:
+            with open('ingredienser.json', 'r', encoding='utf-8') as f:
+                ingredienser_data = json.load(f)
+                for ingrediens_data in ingredienser_data:
+                    namn_ingrediens = ingrediens_data["namn_ingrediens"]
+                    ingreidens_pris = ingrediens_data["ingrediens_pris"]
+                    self.ingredienser.append(Ingrediens(namn_ingrediens, ingreidens_pris))
+        except FileNotFoundError:
+            print("Kunde inte hitta filen 'ingredienser.json'. Kontrollera att filen finns och försök igen.")
 
     def välj_ingredienser(self):
         """
         Låter användaren välja ingredienser för sin sallad.
         """
-        pass
+        for ingrediens in self.ingredienser:
+            print(ingrediens)
 
 
     def hitta_matchande_sallader(self, valda_ingredienser):
@@ -69,7 +87,24 @@ class Salladbar():
         """
         Koordinerar de andra funktionerna för att bearbeta användarens salladval.
         """
-        pass
+        self.läs_in_sallader()
+        self.läs_in_ingredienser()
+
+        while True:
+            print("""
+                Välj ett alternativ från menyn nedan:
+                (a) Skapa en egen sallad.
+                (c) Avsluta programmet.
+                """)
+
+            val = input("Alternativ: ")
+            if val.lower() == 'a':
+                self.välj_ingredienser()
+            elif val.lower() == 'b':
+                break
+            else:
+                print("Ogiltigt val. Försök igen.")
+
 
 def main():
     """
